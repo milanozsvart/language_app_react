d541be9b15d8f521af8df35c13d975ab773e5b58
milanozsvart <64688169+milanozsvart@users.noreply.github.com>
2020-09-04 15:52:02 +0200

    Add files via upload

---
+++ precache-manifest.3fcc7d9c0686bbf64ca1547fe0b86dca.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f926b74cc698049f0682843744005a1f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aa81df1670e4aa32ff27",
+    "url": "./static/css/main.b3778eb5.chunk.css"
+  },
+  {
+    "revision": "efee31868df7a55c5538",
+    "url": "./static/js/2.c241d8b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c241d8b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa81df1670e4aa32ff27",
+    "url": "./static/js/main.75f5826c.chunk.js"
+  },
+  {
+    "revision": "8a987dd47a6dafafc6df",
+    "url": "./static/js/runtime-main.ad3ba77d.js"
+  },
+  {
+    "revision": "ffa68d05e73baa9cc902c60a96ed6027",
+    "url": "./static/media/english-flag.ffa68d05.svg"
+  },
+  {
+    "revision": "549b2c2b42eadf5731a03c27e38c89ed",
+    "url": "./static/media/french-flag.549b2c2b.svg"
+  },
+  {
+    "revision": "7266465c35331005010349d1383d5481",
+    "url": "./static/media/green_pink-bg.7266465c.svg"
+  },
+  {
+    "revision": "4361fb2cd8bb1cb6038acc154cf21d47",
+    "url": "./static/media/green_pink_small.4361fb2c.svg"
+  },
+  {
+    "revision": "f2222739a88dc467b1498867bc1e24d3",
+    "url": "./static/media/hungarian-flag.f2222739.svg"
+  },
+  {
+    "revision": "d1bdd1fafc8acbbd8e33d0b964eee58d",
+    "url": "./static/media/norwegian-flag.d1bdd1fa.svg"
+  },
+  {
+    "revision": "d3d27d6c7327b2e64ae11cc2220e1b2c",
+    "url": "./static/media/polish-flag.d3d27d6c.svg"
+  },
+  {
+    "revision": "35f645bb1c42f809e8bddaf37a1344e4",
+    "url": "./static/media/yla-logo.35f645bb.svg"
+  }
+]);
